ba7c6324e4e7555f4c5928047321c48cb44e0e3c
Lesley-Byte <phosphorescence3@hotmail.com>
2023-11-14 14:27:56 -0500

    better error page component

--- skyway-demo-dash/src/components/Error/index.jsx
+++ skyway-demo-dash/src/components/Error/index.jsx
@@ -1,9 +1,13 @@
-function Error () {
+function Error() {
   return (
-    <div className="error">
-      <p>Error Page.  Something went wrong.</p>
+    <div className="flex justify-center items-center h-screen bg-red-100">
+      <div className="text-center">
+        <p className="text-6xl mb-4">⚠️</p> {/* Emoji as an icon */}
+        <h1 className="text-5xl font-bold text-red-600 mb-3">Error Occurred!</h1>
+        <p className="text-lg text-red-800">Something went wrong. Please try again later.</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Error
+export default Error;
--- skyway-demo-dash/src/components/Home/index.jsx
+++ skyway-demo-dash/src/components/Home/index.jsx
@@ -1,18 +1,34 @@
+// sample custom inline CSS
 function Home() {
   // Sample paragraph text
-  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu ultricies mauris, at facilisis quam. Vivamus et tellus facilisis, vehicula nunc a, fermentum ipsum. ";
+  const text =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu ultricies mauris, at facilisis quam. Vivamus et tellus facilisis, vehicula nunc a, fermentum ipsum. ";
+
+  const fadeInKeyframes = `@keyframes fadeIn {
+    from { opacity: 0; }
+    to { opacity: 1; }
+  }`;
 
   return (
-    <div className="flex justify-center items-center h-screen overflow-auto bg-gray-100">
-      <div className="w-full max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-md">
-        <h1 className="text-3xl font-bold text-center text-gray-900 mb-4">Home</h1>
-        <p className="text-gray-700 text-lg leading-relaxed">{text}</p>
-        {/* Repeating the paragraph to take up more space, like the About page */}
-        <p className="text-gray-700 text-lg leading-relaxed mt-4">{text}</p>
-        <p className="text-gray-700 text-lg leading-relaxed mt-4">{text}</p>
-        {/* You can continue to add more text or other content elements to fill the space as needed */}
+    <>
+      <style>{fadeInKeyframes}</style>
+      <div className="flex justify-center items-center h-screen overflow-auto bg-gray-100">
+        <div
+          style={{ animation: "fadeIn 1s ease-out" }}
+          className="w-full max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-md"
+        >
+          <h1 className="text-3xl font-bold text-center text-gray-900 mb-4">
+            Home
+          </h1>
+
+          <p className="text-gray-700 text-lg leading-relaxed">{text}</p>
+          {/* Repeating the paragraph to take up more space, like the About page */}
+          <p className="text-gray-700 text-lg leading-relaxed mt-4">{text}</p>
+          <p className="text-gray-700 text-lg leading-relaxed mt-4">{text}</p>
+          {/* You can continue to add more text or other content elements to fill the space as needed */}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
